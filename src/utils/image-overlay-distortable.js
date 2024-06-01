f98af1d7156ae9b05c0f78ffc70e8a702d9af1eb
keigo-hashimoto <keigo.hashimoto@i-qps.com>
2024-06-01 17:34:42 +0900

    Fix bug

--- src/utils/image-overlay-distortable.js
+++ src/utils/image-overlay-distortable.js
@@ -1,17 +1,9 @@
 import L from "leaflet";
 import "leaflet-toolbar";
 import "leaflet-distortableimage";
-import loadImage from "easy-image-loader";
 
 export default async function imageOverlayDistortable(url, options) {
   try {
-    const timeout = 5 * 1000; // 5 seconds
-    let img = null;
-    try {
-      img = await loadImage(url, { crossOrigin, timeout });
-    } catch {
-      return null;
-    }
     const lyr = L.distortableImageOverlay(url, options);
     return lyr;
   } catch (error) {
